c243e22660f1e7de9e1b0c49084055e11e149b1d
kethlyn kruk <kethlynsk17@outlook.com>
2024-07-01 21:48:27 -0300

    acima e abaixo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let notas = [7.5, 8.0, 6.0, 9.5, 5.0, 8.5, 7.0, 10.0, 6.5, 9.0]
 let notaMaior = 0
 let notaMenor = notas[0]
+let notasAcimaMedia = 0
+let notaabaixoaMedia = 0
 
 function maiorNota(){
     for (let i = 0; i < notas.length; i++) {
@@ -23,4 +25,23 @@ function menorNota(){
     }
     console.log(menorNota())
 
-    
+    function acima(){
+        for (let i = 0; i < notas.length; i++) {
+        if(notas[i] >= 6) {
+        console.log(notas[i])
+        notasAcimaMedia++
+    }}
+    return notasAcimaMedia
+}
+console.log(acima())
+
+function abaixo(){
+    for (let i = 0; i < notas.length; i++) {
+    if(notas[i] <= 6) {
+    console.log(notas[i])
+    notasAbaixoMedia++
+}}
+return notasAbaixoMedia
+}
+
+console.log(abaixo())
